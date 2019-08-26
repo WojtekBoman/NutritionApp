070d964379eeca1ceac60702f25e162409e0e184
Filip <fwan24@gmail.com>
2019-08-26 17:55:15 +0200

    Dodaj api.js  - obiekt RecipeAPI:  - metoda fetchRecipes przyjmuje obiekt input i zwraca przepisy

---
+++ api.js
@@ -0,0 +1,31 @@
+const RecipeAPI = {
+    _connectionLink: "https://api.edamam.com/search?app_id=ca1ac17c&app_key=055822c03f44a06cf5a82223df807b1b",
+    getConnectionLinkFromInput: function(input) {
+        return this._connectionLink + 
+            (input.q === undefined ? '' : '&q=' + input.q) +
+            (input.from === undefined ? '' : '&from=' + input.from) +
+            (input.to === undefined ? '' : '&to=' + input.to) +
+            (input.ingr === undefined ? '' : '&ingr=' + input.ingr) +
+            (input.diet === undefined ? '' : '&diet=' + input.diet) +
+            (input.health === undefined ? '' : '&health=' + input.health) +
+            (input.cuisineType === undefined ? '' : '&cuisineType=' + input.cuisineType) +
+            (input.mealType === undefined ? '' : '&mealType=' + input.mealType) +
+            (input.dishType === undefined ? '' : '&dishType=' + input.dishType) +
+            (input.calories === undefined ? '' : '&calories=' + input.calories) +
+            (input.time === undefined ? '' : '&time=' + input.time) +
+            (input.excluded === undefined ? '' : '&excluded=' + input.excluded);
+    },
+    fetchRecipes: async function(input) {
+        let conn = this.getConnectionLinkFromInput(input)
+        let recipes = await fetch(conn)
+            .then(response => response.json())
+            .then(data => data.hits.map( d => d.recipe))
+            .catch(err => console.log(err));
+        console.table(recipes);
+        console.log(recipes);
+    }
+}
+
+RecipeAPI.fetchRecipes({
+    q: "fish"
+})
