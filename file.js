2435c9adbca4dc41cd38211bd9867b09818e91a6
Julia Nasiadka <julanasiadka@gmail.com>
2019-09-03 16:05:27 +0200

    function of updating calories

--- file.js
+++ file.js
@@ -1,13 +1,18 @@
 const itemName = document.querySelector("input[type='text']");
+const amount = document.querySelector("input[type='number']");
 const searchItems = document.querySelector(".searchItems"); //form class="searchItems"
 const hintBody = document.querySelector("#listHints"); //ul id="listHints"
 const itemsBody = document.querySelector("#listItems"); //ul id="listItems"
+const caloriesBody = document.querySelector("#caloriesSum"); //div id="caloriesSum"
 
 const items = [];
 const hints = [];
+let caloriesSum = "";
 
 searchItems.addEventListener('submit', searchItem);
-hintBody.addEventListener('click', addItem);
+hintBody.addEventListener('click', addItem); //tu trzeba dodać, żeby wiedział który element listy został naciśnięty
+
+
 
 function searchItem(e) {
     e.preventDefault();
@@ -27,12 +32,21 @@ function searchItem(e) {
     this.reset();
 }
 
-function addItem(e) {
-    //items.push(item); //item, czyli wybrany hint
+function addItem(e) { //dodawanie wybranego hinta do listy itemów
+    //items.push(e); //e, czyli wybrany hint
     const html = items.map(item => {
         return `
         <li>${item.name} <span>${item.calories}</span></li>` //
     });
-    document.querySelector("#listHints").innerHTML = ""; 
+    updateCalories();
+    hintBody.innerHTML = "";
     hints.length = 0; //czyszczenie hints
- }
+
+}
+
+function updateCalories() {
+    caloriesSum = items.map(item => {
+        caloriesSum += item.calories
+    })
+    caloriesBody.innerHtml = caloriesSum;
+}
