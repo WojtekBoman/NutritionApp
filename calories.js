31d2e90bb11f471d87fdf53256018d4e6d02b944
Asedar <miloszstolarczyk@gmail.com>
2019-09-04 15:31:10 +0200

    complete food search

--- calories.js
+++ calories.js
@@ -1,9 +1,11 @@
 class Product
 {
-    constructor(name, calories)
+    constructor(name, calories, amount)
     {
         this.name = name;
         this.calories = calories;
+        this.amount = amount;
+        this.totalCalories = Math.floor((calories / 100) * amount);
     }
 }
 
@@ -27,9 +29,11 @@ async function findProduct()
 
     for(var x = 0; x < 5; x++)
     {
-        temporaryProducts.push(new Product(hints[x].label, Math.floor(hints[x].nutrients.ENERC_KCAL)));
+        temporaryProducts.push(new Product(hints[x].label, Math.floor(hints[x].nutrients.ENERC_KCAL), amount.value));
     }
-    displayHints(temporaryProducts);  
+    displayHints(temporaryProducts);
+    name.value = "";
+    amount.value = "";
 }
 
 function displayHints(temporaryProducts)
@@ -37,15 +41,18 @@ function displayHints(temporaryProducts)
     for(var x = 0; x < temporaryProducts.length; x++)
     {
         var hintElement = document.createElement("li");       
-        hintElement.innerHTML = "<h3>" + temporaryProducts[x].name + "<br>" + temporaryProducts[x].calories + " kcal</h3>"
+        hintElement.innerHTML = "<h3>" + temporaryProducts[x].name + "<br>" + temporaryProducts[x].totalCalories + " kcal</h3>"
         document.getElementById("hints").appendChild(hintElement);
-        hintElement.addEventListener("click", chooseProduct);
+        hintElement.addEventListener("click", function(event)
+        {
+            chooseProduct(event, temporaryProducts);
+        });
     }
 
     document.getElementById("hints").style.display = "flex";
 }
 
-function chooseProduct(event)
+function chooseProduct(event, temporaryProducts)
 {   
     var target = event.target;
     var list = document.getElementById("hints");
@@ -54,5 +61,32 @@ function chooseProduct(event)
         target = target.parentNode;
     }
     var index = Array.prototype.indexOf.call(list.children, target);
-    console.log(index);
+    
+    addProduct(temporaryProducts[index - 1]);
+
+    var parent = document.getElementById("hints");
+    while(parent.children.length >= 2)
+    {
+        parent.removeChild(parent.children[1]);
+    }
+    parent.style.display = "none";
+}
+
+function addProduct(product)
+{
+    products.push(product);
+    document.getElementById("number").innerHTML = parseInt(document.getElementById("number").innerHTML, 10) + product.totalCalories;
+    var productElement = document.createElement("li");       
+    productElement.innerHTML = "<h3>" + product.name + "<br>" + product.totalCalories + " kcal</h3><div style='width: 40px; height: 40px; border: 1px solid; border-radius: 50%; background-image: url(https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-close-round-512.png); background-size: contain;'></div>";
+    document.getElementById("products").appendChild(productElement);
+    productElement.children[1].addEventListener("click", removeItem)
+}
+
+function removeItem(event)
+{
+    var parent = event.target.parentNode;
+    var index = Array.prototype.indexOf.call(parent.parentNode.children, parent);
+    document.getElementById("number").innerHTML = (parseInt(document.getElementById("number").innerHTML, 10) - products[index].totalCalories);
+    products.splice(index, 1);
+    parent.parentNode.removeChild(parent);
 }
